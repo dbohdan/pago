6c2f307fd750a49cb81c4f79fb978b122bd6090f
D. Bohdan <dbohdan@dbohdan.com>
2025-09-22 08:07:55 +0000

    build(version): bump to 0.20.0

--- config.go
+++ config.go
@@ -22,7 +22,7 @@ const (
 	ExitMemlockError = 3
 	FilePerms        = 0o600
 	NameInvalidChars = `[\n]`
-	Version          = "0.19.0"
+	Version          = "0.20.0"
 	WaitForSocket    = 3 * time.Second
 
 	DefaultAgent           = "pago-agent"
