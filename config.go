01b37db2b2d24ea3ca94df4253ac1f32c00049c3
D. Bohdan <dbohdan@dbohdan.com>
2026-05-25 11:36:45 +0000

    build(version): bump to 0.25.0

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ const (
 	MaxStepsPerChar  = 1000 // Maximum attempts to find a random character matching apattern.
 	NameInvalidChars = `[\p{Cc}]`
 	StorePath        = "store"
-	Version          = "0.24.1"
+	Version          = "0.25.0"
 	WaitForSocket    = 3 * time.Second
 
 	ExitOK           = 0
